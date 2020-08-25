420f593097f9b2132fea8849b86c6ae2b9432309
Harsh-commits <58255681+Harsh-commits@users.noreply.github.com>
2020-08-25 22:22:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
    <?php include 'links.php' ?>
    <?php include 'Style.php' ?>
    <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta name="google-site-verification" content="WwrWNB959CnIdDy4fH7KU7lxDJXFVA_6KG_oNaTzK7o" />
+  
+   <meta name="google-site-verification" content="FSzuDBbQrVVWzB5xUP5_xiRYmZfjHwrDRDKekRtcUlc" />
   
    <meta name="description" content="Latest COVID-19 pandemic updates of India. Live stats , Day wise report , state wise report , Latest MHA guidelines , COVID help as on particular date.">
    <meta name="keywords" content="Corona , Covid-19 , Symptoms , About corona, preventions , Guidelines ">
